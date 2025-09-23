2801b7754cbf2d61694332c5bf1323c7c936bdd2
tarek-code <tarekadel314@gmail.com>
2025-09-23 21:54:17 +0300

    Add VPC Access Connector for Cloud Run to connect to Redis and update environment variables for Redis connection in Terraform configuration.

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,22 +21,34 @@ resource "google_redis_instance" "my_redis" {
   region         = "us-central1"
 }
 
+# Serverless VPC Access connector for Cloud Run to reach Memorystore (default VPC)
+resource "google_vpc_access_connector" "run_connector" {
+  name          = "run-to-redis"
+  region        = "us-central1"
+  network       = "default"
+  ip_cidr_range = "10.8.0.0/28"
+}
+
 # Cloud Run Service
 resource "google_cloud_run_service" "my_service" {
   name     = "teamavail-cloudrun-service"
   location = "us-central1"
 
   template {
+    metadata {
+      annotations = {
+        "run.googleapis.com/vpc-access-connector" = google_vpc_access_connector.run_connector.id
+        "run.googleapis.com/vpc-access-egress"    = "all-traffic"
+      }
+    }
     spec {
+      timeout_seconds = 300
+
       containers {
         image = "docker.io/tarekadel/teamavail:latest"
         env {
-          name  = "REDIS_HOST"
-          value = google_redis_instance.my_redis.host
-        }
-        env {
-          name  = "REDIS_PORT"
-          value = google_redis_instance.my_redis.port
+          name  = "REDIS_URL"
+          value = "redis://${google_redis_instance.my_redis.host}:${google_redis_instance.my_redis.port}"
         }
       }
     }
